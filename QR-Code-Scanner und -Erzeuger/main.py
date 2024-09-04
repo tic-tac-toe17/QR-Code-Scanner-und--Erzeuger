6de323546a757e5be7964077f221c0706ddf3347
marekgiese <marekgi@tutanota.com>
2024-09-04 11:48:55 +0200

    Created main.py Created README.md Created requirements.txt

--- QR-Code-Scanner und -Erzeuger/main.py
+++ QR-Code-Scanner und -Erzeuger/main.py
@@ -1,1 +1,31 @@
-print("QR-Code")
+import cv2
+import qrcode
+
+from PyQt5.QtWidgets import *
+from PyQt5 import uic, QtGui
+
+class Program(QMainWindow):
+
+    def __init__(self):
+        pass
+
+    def load_image(self):
+        pass
+
+    def save_image(self):
+        pass
+
+    def generate_code(self):
+        pass
+
+    def read_code(self):
+        pass
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
